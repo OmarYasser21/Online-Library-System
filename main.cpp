c973cb28b8bfb9f3410d8047b80e44334dc436bb
OmarYasser21 <94626701+OmarYasser21@users.noreply.github.com>
2022-06-13 18:33:14 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "User.h"
+#include "Book.h"
+#include "UserList.h"
+#include "BookList.h"
+
+using namespace std;
+
+int main()
+{
+    int choice;
+    cout << "Select one of the following choices: \n 1-Users menu \n 2-Books menu \n 3-Exit" << endl;
+
+    return 0;
+}
